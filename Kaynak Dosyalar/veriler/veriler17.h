3a5012f1b039d0201387740a27af94e792633950
Gokhan1717 <49857128+Gokhan1717@users.noreply.github.com>
2019-04-23 21:05:21 +0300

    PROJE 17-18-19
    FIRINDAN YENİ ÇIKTI (GÖKHAN)

---
+++ Kaynak Dosyalar/veriler/veriler17.h
@@ -0,0 +1,32 @@
+#include "namespace.h"
+using namespace TUBITAK;
+
+class proje17 {
+	public:
+		string baslik17;
+		string kullanilan_materyaller17;
+		string metot17;
+		string ozet17;
+		string soru17;
+		string hipotez17;
+                string konu_arastirmasi17;
+                string sonuclar17;
+		string sonuc_ve_tartisma17;
+	
+
+		
+		proje17() {
+		baslik17="VÝTAMÝNLÝ REJENERASYON";	
+		kullanilan_materyaller17="2 adet toprak solucaný 2 özdeþ beher Su Vitamin (Karma) Cetvel hassas tartý";
+		metot17="Topraktan iki adet toprak solucaný buduk. Birni 0.9 gram ve 9 cm, diðerini ise 0.7 gram 12 cm olarak ölçtük. Topraktan bulmuþ olduðumuz bu iki adet toprak solucanýný ikiye böldük. 0.4 ve 0.5 gram olacak þekilde iki solucan parçasý elde ettik. Ýki solucanýn kafa kýsmýný kullandýk. Ýki kaba da eþit miktarda toprak koyduk. Seçtiðimiz bir solucanýn topraðýna bir miktar vitamin ve su ekledik. Diðer solucanýn topraðýna ise sadece su ekledik. Daha sonra düzenli olarak iki günde bir solucanlarýmýzý kontrol edip, vitamin ve su takviyesi yaptýk. Ýlk gün vitaminsiz solucan zayýflarken, vitaminli solucanda herhangi bir deðiþim gözlenmedi.";
+		ozet17="Günlük yaþantýmýzda geçirdiðimiz ufacýk kazalar maalesef bazen bizler için bir takým sorunlara yol açmakta. Normal derecede bir açýk yaranýn kapanmasý iki, üç gün almaktadýr. Bu süreç zarfýnda açýk yaralar iltihaplanma sonucunda ise yüksek ateþlenme sonucu dirençsiz vücut bebeklerde kalýcý hasara yol açmaktadýr. Bende bunu engellemek amacý ile rejenerasyon nasýl etkilediðini ölçen bir deney yaptým.";
+		soru17="Vitaminler rejenerasyon üzerinde etkili midir?";
+		hipotez17="Vitaminler rejenerasyon hýzýný arttýrýr.";
+		konu_arastirmasi17="Türk edebiyatýnda iz býrakmýþ önemli þairlerin hayatlarýný derinden etkileyen olaylarý araþtýrmak ve bu olaylarýn onlarýn sanat anlayýþlarýna özellikle de þiirlerine ne gibi etkileri olduðunu tespit etmek.";
+        sonuclar17="Türk edebiyatýnda iz býrakmýþ önemli þairlerin erken yaþta annelerini yitirmeleri ve bu ayrýlýktan sonra yaþadýklarý boþluk hissi, onlarý büyük ölçüde etkilemiþtir. Bu sanatçýlarýn þiirlerinde "anne"nin çoðu kez yad edildiðini yahut ölüm ve hüzün temalarýyla kendini gösterdiðini tespit etmekteyiz. Bu kutsal varlýk onlar için;bir zamanlar merhamet dolu kalbiyle huzur ve güven bahþeden fakat erken yitirilmiþ bir sýðýnaktýr. Eksikliðini hiçbir zaman kapatýlmamýþ, bu özlem; þiirlerde imgesel bir boyutta ortaya çýkmýþtýr. Bu deðerli varlýðý topraða emanet eden yahut onun þefkatinden yoksun büyüyen þair için, artýk toprak da kutsaldýr. Dolayýsýyla ölüm, anlamda anneye dönüþtür.";
+        sonuc_ve_tartisma17=" Bir hafta süren bu"Vitaminli Rejenerasyon" deneyi sonucunda vitaminli solucanýmýz rejenerasyonu daha hýzlý ve saðlýklý bir biçiminde tamamlarken, vitaminsiz solucanýmýz bu süreci daha yavaþ ve hareketsiz tamamladý.Bu deneyin sonucunda vitamin rejenerasyonu yani asýl amaç olan açýk yaralarýn kapanmasýný hýzlandýrdý. Bu sebeple amaçladýðýmýz sonuca ulaþýp vitaminli yara bandý yapýmýnýn kalýcý etkisi olduðunu, iltihap oluþumunun neden olduðu kalýcý hasarlarý önlemeye yönelik de bir etkisi olduðunu gözlemledik.";
+
+		}
+};
+
+
