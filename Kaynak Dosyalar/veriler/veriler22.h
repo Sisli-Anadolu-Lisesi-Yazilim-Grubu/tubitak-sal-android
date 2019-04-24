10871062fb1c37c9908233dd8b94853762013ff6
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-24 13:49:04 +0300

    veriler22
    göchkan senin ellerin dert görmesin

--- Kaynak Dosyalar/veriler/veriler22.h
+++ Kaynak Dosyalar/veriler/veriler22.h
@@ -11,21 +11,15 @@ class proje22 {
 		string hipotez22;
 		string konu_arastirmasi22;
 		string analiz22;
-        proje22() {
-			baslik22 = "TÜBİTAK 4006 PROJESİ KOMPOST OLUŞUMUNDA ŞARTLARIN ETKİSİ";
-            kullanilan_materyaller22="Doğal atıklar(Sebze ve Meyve Atıkları)Kazma,Kürek,Renksiz Karton";
-
-
-			proje_yontemi22 = "Proje kapsamında Permakültür Tarım yapabilmek için kompost hazırladık. Hazırladığımız kompostu üç alanda yaptık. Bunlar
-Varilde kompostta
-Toprağın içinde kompost
-Toprak yüzeyinde(dışarıda) kompost
-Bu üç kompost yapımı içim süre ve eşit miktarda organik atık kullandık. Kompost yapımı ve permakültür tarım için okulumuzun kapalı spor salonunun arkasındaki boş arsayı kullandık.Öncelikle kendi evlerimizdeki evsel gıda atıklarımızı depolayıp okula getirdik. Bu atıkların içinde pişmiş yemek, ekmek, portakal-limon kabuğu ve soğan kabuğu yoktu. Meyve-sebze atıkları, çay-kahve artığı, karton, ağaç yaprakları kompost yapımı için  uygun atıklardır. Bu atıkları gerek evimizden getirmek suretiyle gerekse okul kantini ve öğretmenlerimizin kullandığı mutfaktan temin ettik. Ağaç yapraklarını ise okulumuz bahçesindeki ağaçların düşen yapraklarını toplayarak temin ettik.
-Kompost hazırlama sürecinde öncelikle karton atıklarımızı kompostumuzun en alt kısmına yerleştirdik. Karton atıklarının üzerine gıda artıklarını, onun üzerine ağaç yapraklarını en üste ise çay ve kahve atıklarını ekledik. Tabakalar halinde koyduğumuz bu artıkları iki günde bir karıştırmaya çalıştık. Evlerimizden getirdiğimiz veya okuldan temin ettiğimiz artık maddeleri kompostumuzun üzerine ekledik. Her eklemeden sonra kompostumuzu karıştırdık. Karıştırma sırasında kompostumuza su da ekledik.Yaklaşık üç ay boyunca eklemelerle hazırladığımız kompostlarımızın yapım sürecini fotoğraf çekmek suretiyle gözlemleyerek kayıt altına aldık. Üç ayrı alanda kompost hazırladığımız için bunları üç farklı yarım yatağında deneyeceğiz.
-";
-			soru22 = "Kompost oluşumunda ortam ve dış faktörlerin etkisi fazla mıdır?";
-			hipotez22 = "Kompost hazırlanmasında ve kompostun en hızlı  ve en verimli oluşturulacağı hakkında bilgi edinmek.";
-        konu_arastirmasi22="Günlük hayatta organik atıklarımızı nasıl verimli hale getirebiliriz ve tarım için kullanabiliriz, bu sayede organik tarım konusunda da bilinçlenmiş olacağız.";
-		analiz22="Kompostların yapım sürecinde toprağın içinde yaptığımız kompostun daha hızlı geliştiğini gözlemledik. En yavaş ise yer üstünde olanıydı. Toprakta canlı organizmanın(solucan,bakteri vb.)daha çok olmasından dolayı diğerlerine göre toprağın içindeki kompostumuz daha hızlı gelişmiştir. Varildeki kompostumuza toprak altındaki canlı organizmalar ulaşamacağından yer altındakine göre daha yavaş geliştiğini gözlemledik. Yer üstündeki kompostumuz ise açık alanda olduğu için ısısını ve nemini koruyamadı. Nem ve sıcaklık enzimlerin ve canlı organizmaların yaşayabilmesi için gerek bir etken olduğundan diğer kompostlarımıza göre geride kaldı.";
+     
+	proje22() {
+		
+	baslik22 = "TÜBİTAK 4006 PROJESİ KOMPOST OLUŞUMUNDA ŞARTLARIN ETKİSİ";
+	kullanilan_materyaller22="Doğal atıklar(Sebze ve Meyve Atıkları)Kazma,Kürek,Renksiz Karton";
+	proje_yontemi22 = "Proje kapsamında Permakültür Tarım yapabilmek için kompost hazırladık. Hazırladığımız kompostu üç alanda yaptık. Bunlar Varilde kompostta Toprağın içinde kompost Toprak yüzeyinde(dışarıda) kompost Bu üç kompost yapımı içim süre ve eşit miktarda organik atık kullandık. Kompost yapımı ve permakültür tarım için okulumuzun kapalı spor salonunun arkasındaki boş arsayı kullandık.Öncelikle kendi evlerimizdeki evsel gıda atıklarımızı depolayıp okula getirdik. Bu atıkların içinde pişmiş yemek, ekmek, portakal-limon kabuğu ve soğan kabuğu yoktu. Meyve-sebze atıkları, çay-kahve artığı, karton, ağaç yaprakları kompost yapımı için  uygun atıklardır. Bu atıkları gerek evimizden getirmek suretiyle gerekse okul kantini ve öğretmenlerimizin kullandığı mutfaktan temin ettik. Ağaç yapraklarını ise okulumuz bahçesindeki ağaçların düşen yapraklarını toplayarak temin ettik. Kompost hazırlama sürecinde öncelikle karton atıklarımızı kompostumuzun en alt kısmına yerleştirdik. Karton atıklarının üzerine gıda artıklarını, onun üzerine ağaç yapraklarını en üste ise çay ve kahve atıklarını ekledik. Tabakalar halinde koyduğumuz bu artıkları iki günde bir karıştırmaya çalıştık. Evlerimizden getirdiğimiz veya okuldan temin ettiğimiz artık maddeleri kompostumuzun üzerine ekledik. Her eklemeden sonra kompostumuzu karıştırdık. Karıştırma sırasında kompostumuza su da ekledik.Yaklaşık üç ay boyunca eklemelerle hazırladığımız kompostlarımızın yapım sürecini fotoğraf çekmek suretiyle gözlemleyerek kayıt altına aldık. Üç ayrı alanda kompost hazırladığımız için bunları üç farklı yarım yatağında deneyeceğiz.";
+	soru22 = "Kompost oluşumunda ortam ve dış faktörlerin etkisi fazla mıdır?";
+	hipotez22 = "Kompost hazırlanmasında ve kompostun en hızlı  ve en verimli oluşturulacağı hakkında bilgi edinmek.";
+	konu_arastirmasi22="Günlük hayatta organik atıklarımızı nasıl verimli hale getirebiliriz ve tarım için kullanabiliriz, bu sayede organik tarım konusunda da bilinçlenmiş olacağız.";
+	analiz22="Kompostların yapım sürecinde toprağın içinde yaptığımız kompostun daha hızlı geliştiğini gözlemledik. En yavaş ise yer üstünde olanıydı. Toprakta canlı organizmanın(solucan,bakteri vb.)daha çok olmasından dolayı diğerlerine göre toprağın içindeki kompostumuz daha hızlı gelişmiştir. Varildeki kompostumuza toprak altındaki canlı organizmalar ulaşamacağından yer altındakine göre daha yavaş geliştiğini gözlemledik. Yer üstündeki kompostumuz ise açık alanda olduğu için ısısını ve nemini koruyamadı. Nem ve sıcaklık enzimlerin ve canlı organizmaların yaşayabilmesi için gerek bir etken olduğundan diğer kompostlarımıza göre geride kaldı.";
 		}
 };
