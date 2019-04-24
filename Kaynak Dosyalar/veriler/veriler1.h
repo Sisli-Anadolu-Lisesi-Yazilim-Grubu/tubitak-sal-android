c8169290244a2eee9dbbc01f76b36bb80b566397
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-24 13:55:16 +0300

    by Fatih düzeldi

--- Kaynak Dosyalar/veriler/veriler1.h
+++ Kaynak Dosyalar/veriler/veriler1.h
@@ -4,16 +4,18 @@ class proje1 {
 	public:
 		string baslik ; 
 		string kullanilan_materyaller ;
-		string uygulanan_yontemler,analiz_sonuclari ;
+		string uygulanan_yontemler ;
+		string analiz_sonuclari ;
 		string ozet; 
 		string oneriler_ve_beklentiler;
 		
 		proje1(){
 			baslik = "ŞİŞLİ'NİN DÜNÜ VE BUGÜNÜ";
 			kullanilan_materyaller = "Fotoğraf Makinesi";
-			uygulanan_yontemler,analiz_sonuclari = "Gezi - Gözlem - Araştırma";
+			uygulanan_yontemler = "Gezi - Gözlem - Araştırma";
+			analiz_sonuclari =  "Şişli de tarihi dokunun korunamadığı; AVM ve gökdelenlerin sayısının fazla olduğu, yeşil alanın az olduğu görülmüştür.";
 			ozet = "İstanbul tarih boyunca önemli bir şehir olmuştur.Şişli de İstanbul'un önemli semtlerinden biridir.Bu proje ile tarih boyunca Şişli'nin geçirdiği dönüşümü ortaya çıkarmayı amaçlıyoruz";
-			oneriler_ve_beklentiler = " Şişli'de AVM ve gökdelenlerin çokolduğu için yüksek binaların yapılması önlenmelidir. alanların çoğaltılması gerekmektedir. Otopark sorununu çözmek için yer altına otopark yapılmalıdır. Dikey mimari yerine yatay mimariyeönem verilmelidir.";
+			oneriler_ve_beklentiler = " Şişli'de AVM ve gökdelenler çok olduğu için yüksek binaların yapılması önlenmelidir. Yeşil alanların çoğaltılması gerekmektedir. Otopark sorununu çözmek için yer altına otopark yapılmalıdır. Dikey mimari yerine yatay mimariye önem verilmelidir.";
 			
 		}
 };
