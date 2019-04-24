8b3e0b01a7becbbdcdf4f024a68925443b50a0e8
Samet Sevindik <SametHUB732@gmail.com>
2019-04-24 13:28:36 +0300

    Veriler 21 (""BİTMEDİ!!!)
    BİRİSİ BİTİRSİN İŞİM VAR.d

---
+++ Kaynak Dosyalar/veriler/veriler21.h
@@ -0,0 +1,31 @@
+#include "namespace.h"
+
+using namespace TUBITAK;
+
+class proje21 {
+public:
+    string baslik21;
+    string materyaller21;
+    string yontemler21;
+    string analiz_sonuclari21;
+    string ozet21;
+    string soru21;
+    string hipotez21;
+    string arastirma21;
+    string sonuc21;
+    string beklenti21;
+    string oneri21;
+
+    proje21() {
+        baslik21 = "MATEMATİKTE OLASILIK KONUSU HAKKINDA OYUN GELİŞTİRME";
+        materyaller21 = "Yazı tahtası, Tahta kalemi";
+        yontemler21 = "Matematikte olasılık konusununun öğrenimi için geliştirdiğimiz oyun iki öğrenci ya da iki grup tarafından oynanabilmektedir. Oyunun temel amacı karşılıklı tutulan dört basamaklı sayıyı bulabilmektedir. Sayının bulunması için yapılan tahminde basamak olarak doğru yerde bilinen rakam için (+). basamağı yanlış yerde bilinen rakam için (-), yanlıi tahmin edilen rakamlar (0) kullanılarak sayı bulunmaya çalışılır. Birden fazla rakamın basamağı yanlış yerde fakat doğru tahmin edildiğinde eksi işaretlerinin sayısı artırılır. Birden fazla rakamın basamağı yanlış fakat doğru tahmin edildiğinde eksi işaretlerinin sayısı artırılır. Sayılar birbirinden farklı olmalıdır. Yarışan öğrencilerin tahminleri doğru değerlendirmeleri gerekmektedir. Eğer karşı grubu yanlış yönlendirirlerse hükmen yenik sayılırlar. Her tahminde sayıların yerlerini değiştirerek ve kombinasyon-olasılık mantığını kullanarak sayıyı en az tahminle bulan kazanır. Dolayısıyla oyunun zorluk derecesi sayının basamak sayısı artırıp azaltılarak öğrencilerin seviyelerine göre ayarlanabilir.";
+        analiz_sonuclari21  = "10 öğrenci üzerinde gözlemler yapılmış, öğrencilerin ortak noktası üçüncü oyundan sonra tahmin etmeyi bırakıp olasılık hesabı yaparak oyundaki üstünlükleri artmıştır.Buda  gösteriyor ki akran öğreniminin birleşmesi ile oluşturduğumuz oyun, belirli bir süreden sonra eğlenceden çok bir rekabete dönüştüğü için üstünlük sağlamak adına başka yöntemlere başvurulmuştur. Öğrencilerin olasılık hesabını kavradıkları gözlemlenmiştir.";
+        ozet21 = "Matematikte olasılık konusu genellikle öğrenciler tarafından zor anlaşılan bir konu olup çoğu zaman konuyu tam olarak anladığını düşünen öğrenciler için bile kafa karıştırıcı olabilmektedir. Matematikte oyunla öğrenme sıkça başvurulan bir yöntem olup olasılık konusunda da diğer konulara nazaran pek fazla çalışma bulunmamaktadır. Yapacağımız araştırmalarda olasılık konusunda keyif verici bir oyuna rastlayamadık ve bu yüzden böyle bir oyun geliştirmenin olasılık eğitimi konusunda yararlı olabileceğini düşündük. Hazırladığımız oyunda kubaşık öğrenme yöntemi kullandık. Bu yöntemde öğrenciler dört basamaklı bir sayıyı karşılıklı olarak birbirlerine göstermeden belirlerler. Öğrenciler tahminlerle ve olasılık biliminden faydalanarak karşılıklı şekilde sayıları bulmaya çalışırlar. Önce bulan öğrenci ise oyunu kazanmış olur. Böylece keyif verici, rekabetçi ve sosyal bir öğrenme ortamında olasılık konusuyla alıştırma yapmış olurlar. Matematik konusunda hazırladığımız bir oyunun diğer konularda oyunla öğrenmeyi de teşvik edeceğini düşünüyoruz.";
+        sonuc21 = "OLASILIK KONUSUNU NASIL DAHA KEYİF VERİCİ ŞEKİLDE ÖĞRENEBİLİRİZ?";
+        hipotez21 = "Olasılık konusunu oyun ve akran öğrenimi ile birleştirirsek daha rahat öğrenebiliriz.";
+        arastirma21 = "Günümüzde birçok matematik konularını çalışmak yerine zorluğundan yakınıyor ve kalan boş zamanlarını da oyunların başında geçiriyor. Oyunlarla ayırdıkları vakit azalmazsa hem kendileri hem de ülkemiz matematik konusunda geri kalacak.";
+        sonuc21  = "Bu oyun sayesinde: Öğrencilerin hızlı düşünebilme özelliği gelişim göstermiştir. Olasılık hesabını kavrayabilme yetenekleri artmıştır. Matematik dersine olan ilgilerinin artması sağlanmıştır. Kubaşık öğrenme yöntemi ile öğrencinin olasılık hesabını öğrenmesi kolaylaşmıştır. Oyunun öğrenime katkısı olduğu gözlemlenmiştir. Eğlence öğrencilerde olumlu etki yaratmış ve özgüvenlerini geliştirmiştir.";
+        bek
+    }
+};
