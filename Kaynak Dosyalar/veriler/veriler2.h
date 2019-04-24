a5fe0f54b5cd9fe1ff3d318798bae921a3cba9f7
tatarmehmed <45494918+saldogaproje@users.noreply.github.com>
2019-04-24 12:29:11 +0300

    DÜZELTİLDİ BY GÖKHAN
    Yanlış kısımlar düzeltildi

--- Kaynak Dosyalar/veriler/veriler2.h
+++ Kaynak Dosyalar/veriler/veriler2.h
@@ -14,7 +14,7 @@ string hipotez2;
 
 proje2(){
 baslik2 ="ARITILAN BAKTERİ ";
-materyaller2= "•BİR MİKTAR TOPRAK •4 ADET KAP •SÜZGEÇ •PETRİ KABI(AGAR) •HUNİ •GAZLI BEZ •AKTİF KARBON •KARBONAT •ALÜMİNYUMUM FOLYO •STREÇ FİLM •BIÇAK •MAKAS"
+materyaller2= "BİR MİKTAR TOPRAK 4 ADET KAP SÜZGEÇ PETRİ KABI(AGAR) HUNİ GAZLI BEZ AKTİF KARBON KARBONAT ALÜMİNYUMUM FOLYO STREÇ FİLM BIÇAK MAKAS";
 metot2 = "Doğadan aldığımız bir miktar toprağı bir hafta boyunca kalın taneciklerinden süzülmesi için beklettik.Bir haftanın sonunda elde edilen suya süzgeç ile fiziksel süzme işlemi yaptık.Elimizdeki suyu petri kaplarında katı besiyerine (agar) ektik( kontrol grubu). Ardından bir huniye bakteri arıtma düzeneği kurduk.Huninin içine bir tabaka gazlı bez ve üstüne aktif karbon yerleştirdik.Diğer tabakaya yine bir gazlı bezin içine karbonat koyduk ve en üst katmana ise tekrar gazlı bez ve aktif karbon kullandık.Elimizdeki fiziksel süzülmüş suyu bu huniden geçirerek ikinci bir filtre yapmış olduk.Bu sudan aldığımız örneği de agar besiyerine ektik.En son adımda ise elde ettiğimiz suyu kaynatarak steril etmeye, yani mikroorganizmalardan arındırmaya çalıştık.Bu sudan aldığımız örneği de agar besiyerine ektik.Agar besiyerlerini uygun sıcaklık koşullarında (folyo sararak) beklemeye bıraktık. ";
 ozet2 = "Tüm canlılar için olduğu gibi su insanlar için de oldukça önemli bir yaşam kaynağıdır.İçme suyu ya da şebeke suyu olarak bize yaşamımızda pek çok fayda sağlar.Günlük yaşamımızda sıklıkla kullandığımız suyu sürekli olarak yerine koymak hem maddi hem de manevi olarak yorucu olabilmektedir.Biz de bu yüzden suyu bir takım aşamalardan geçirerek arıtmaya çalıştık.";	
 soru2 = "Kirli suyu nasıl arıtabiliriz?";
