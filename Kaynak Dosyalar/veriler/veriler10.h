60ac9c87058eaecbfdb93a21ed6ced3e896943fd
tatarmehmed <45494918+saldogaproje@users.noreply.github.com>
2019-04-24 12:38:03 +0300

    DÜZELTİLDİ BY GÖKHAN
    Yanlışlıklar düzeltildi

--- Kaynak Dosyalar/veriler/veriler10.h
+++ Kaynak Dosyalar/veriler/veriler10.h
@@ -5,7 +5,7 @@ using namespace TUBITAK;
 class proje10 {
 	public:
 		string baslik10;
-		string kullanilen_materyaller10;
+		string kullanilan_materyaller10;
 		string uygulanan_yontem10;
 		string analiz_sonuclari10;
 		string proje_ozeti10;
@@ -16,13 +16,13 @@ class proje10 {
 		
 		proje10() {
 			
-		baslikr10="YÜRÜYEN KRİSTALLER";
-		kullanilen_materyaller10="Birkaç adet piezo, Li-Po batarya, akım dönüştürücü için 4 adet diyot, kullanılmayan geniş tabanlı bir spor ayakkabı kullandık.";
+		baslik10="YÜRÜYEN KRİSTALLER";
+		kullanilan_materyaller10="Birkaç adet piezo, Li-Po batarya, akım dönüştürücü için 4 adet diyot, kullanılmayan geniş tabanlı bir spor ayakkabı kullandık.";
 		uygulanan_yontem10="Devremizi  tamamladıktan sonra ayakkabının tabanına uygun bir şekilde yerleştirdik. Daha sonra yürüyüş yaparak test ettik. Bataryanın sarj olup olmadığından emin olmak için yürüyüşten sonra bir led ile kontrol ettik.";
 		analiz_sonuclari10="Test sonucuna göre piezolar ile elektrik üretmenin mümkün olduğunda kanıtlamış olduk. Daha iyi bir tasarım ve devre ile bu fikrin ileride, günlük hayatta önemli bir yeri olacağıma inanıyoruz.";
 		proje_ozeti10="Elektrik üretmenin alternarif yollarını ararken, piezo kristallerin üretebileceğini fark ettik. Bunun üzerine tasarladığımız ayakkabıda, bu yöntem ile günlük hareketlerimizden elektrik üreterek bunu kullanmayı amaçladık.";
 		hipotez10="Piezo kristalleri kullanarak, ekstra efor sarf etmeden enerji üretebilir miyiz ?";
-		konu_atastirmasi10="Genel olarak titreşim sensörü olarak kullanılan piezolar, üstüne basınç uygulandığına küçük bir miktarda elektrik üretiyor. Bu elektriği depolayıp, kullanma açısından pek çok çalışma olmadığından, kaynağımız sınırlı.";
+		konu_arastirmasi10="Genel olarak titreşim sensörü olarak kullanılan piezolar, üstüne basınç uygulandığına küçük bir miktarda elektrik üretiyor. Bu elektriği depolayıp, kullanma açısından pek çok çalışma olmadığından, kaynağımız sınırlı.";
 		sonuclar10="Piezo kristaller ilke elektrik üretip bu elektriği kullanabileceğimizi kanıtladık.";
 		oneri_ve_beklentiler="Bu teknolojinin ileride kullanım alanının geniş olacağına inanıyoruz. Bu tasarım geliştirilirse kişinin günlük hayatında önemli yer kaplayacağını düşünüyoruz. Sadece ayakkabı ile kalmayıp, örneğin kalabalık yerledeki yollarda kullanılabilir.";
 		}
