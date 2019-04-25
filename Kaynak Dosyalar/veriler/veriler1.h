614a630bb7965c2984fc1b6457501c73e630e7fd
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-25 15:39:28 +0300

    Update veriler1.h

--- Kaynak Dosyalar/veriler/veriler1.h
+++ Kaynak Dosyalar/veriler/veriler1.h
@@ -14,7 +14,7 @@ class proje1 {
 			kullanilan_materyaller = "Fotoğraf Makinesi";
 			uygulanan_yontemler = "Gezi - Gözlem - Araştırma";
 			analiz_sonuclari =  "Şişli de tarihi dokunun korunamadığı; AVM ve gökdelenlerin sayısının fazla olduğu, yeşil alanın az olduğu görülmüştür.";
-			ozet = "İstanbul tarih boyunca önemli bir şehir olmuştur.Şişli de İstanbul'un önemli semtlerinden biridir.Bu proje ile tarih boyunca Şişli'nin geçirdiği dönüşümü ortaya çıkarmayı amaçlıyoruz";
+			ozet = "İstanbul tarih boyunca önemli bir şehir \n olmuştur.Şişli de İstanbul'un önemli \n semtlerinden biridir.Bu proje ile tarih boyunca Şişli'nin geçirdiği dönüşümü ortaya çıkarmayı amaçlıyoruz";
 			oneriler_ve_beklentiler = " Şişli'de AVM ve gökdelenler çok olduğu için yüksek binaların yapılması önlenmelidir. Yeşil alanların çoğaltılması gerekmektedir. Otopark sorununu çözmek için yer altına otopark yapılmalıdır. Dikey mimari yerine yatay mimariye önem verilmelidir.";
 			
 		}
