9b142ce33cb84463e1a8e9db9d3ca9f55161c937
Gokhan1717 <49857128+Gokhan1717@users.noreply.github.com>
2019-04-25 21:22:03 +0300

    veriler13.h \n eklendi
    \n eklendi

--- Kaynak Dosyalar/veriler/veriler13.h
+++ Kaynak Dosyalar/veriler/veriler13.h
@@ -14,15 +14,15 @@ public:
     string sonuc13;
     string oneriler13;
 
-    proje() {
-        baslik13 = "KIRMIZI MAVİ";
-        materyaller13 = "Demir çubuk Termometre Çakmak Tüp";
-        yontem13 = "Kırmızı ve mavi renklerini sadece hoş görüntü sağlayabilmesi için mi kullanıyoruz ? Yapacak olduğumuz bu deneyde her şeyin aynı olup yalnızca renklerin farklı olmasını sağlayacağız. Bir ortam hazırlayacağız ardından mavi ve kırmızı renkteki demirlere aynı şiddette ısılar vereceğiz. Kırmızı ve mavinin ısı iletimini ve iletkenlikleri arasındaki farkı ölçeceğiz. Bireylerin ufak ufak ısıdan tasarruf etmesi dünya genelindeki insan sayısını düşündüğümüzde önemli bir etki yaratır. Eğer bu ısı yalıtımını azaltmış oluruz hem de doğayı korumuş oluruz.";
-        analiz_sonuc13 = "Aynı koşullarda farklı renklerin ısı iletkenliklerinin farklı olduğu tespit  edildi.";
-        ozet13 = "Aynı cins maddeden yapılmış enleri , yükseklikleri ve kalınlıkları eşit iki çubuk kırmızı ve mavi renklere boyanır. İki demirin aynı ortam koşullarında ısı iletim değişikleri incelenir. Bu deneyden beklenti iki demirin de ısı iletiminin eşit miktarsa olmasıdır. Deneyden elde edilen bilgilere göre yaşantımızdaki ısı tasarrufunu artırmak, veriler ile günlük hayat şartlarının kolaylaştırmak amaçlanır.";
-        hipotez13 = "Mavi ve kırmızı çubukları alıp ısıtacağız. Hangisini daha iyi ısıyı ilettiğini gözlemleyip bunun üzerinden çeşitli çözümler üreteceğiz.";
-        arastirma13 = "Mavi ve kırmızı çubukların eşit koşullarda ısıyı aynı oranda iletip iletmediğini araştırdık. Isının renkler üzerinde etkisinin nasıl olduğuna dair çalışmalar yaptık.";
-        sonuc13 = "Başta 22° sıcaklığa sahip iki çubuk demir bulunmaktadır. Çubuk tutucularına yerleştirilen çubukları aynı koşullarda ısıtılır. Mavi boyalı demirin sıcaklığı 55° olarak ölçülmüştür.";
-        oneriler13 = "Mavi renge boyanan enerji nakil hatlarını ısı iletimleri tasarruf açısından daha etkilidir. Bu yüzden materyallerin mavi renge boyanması beklenir. ";
+    proje13() {
+        baslik13 = "KIRMIZI MAVİ\n";
+        materyaller13 = "Demir çubuk , Termometre , Çakmak , Tüp\n";
+        yontem13 = "Kırmızı ve mavi renklerini sadece hoş görüntü sağlayabilmesi için mi kullanıyoruz ?\n Yapacak olduğumuz bu deneyde her şeyin aynı olup yalnızca renklerin farklı olmasını sağlayacağız.\n Bir ortam hazırlayacağız ardından mavi ve kırmızı renkteki demirlere aynı şiddette ısılar vereceğiz.\n Kırmızı ve mavinin ısı iletimini ve iletkenlikleri arasındaki farkı ölçeceğiz.\n Bireylerin ufak ufak ısıdan tasarruf etmesi dünya genelindeki insan sayısını düşündüğümüzde önemli bir etki yaratır.\n Eğer bu ısı yalıtımını azaltmış oluruz hem de doğayı korumuş oluruz.\n";
+        analiz_sonuc13 = "Aynı koşullarda farklı renklerin ısı iletkenliklerinin farklı olduğu tespit  edildi.\n";
+        ozet13 = "Aynı cins maddeden yapılmış enleri , yükseklikleri ve kalınlıkları eşit iki çubuk kırmızı ve mavi renklere boyanır.\n İki demirin aynı ortam koşullarında ısı iletim değişikleri incelenir.\n Bu deneyden beklenti iki demirin de ısı iletiminin eşit miktarsa olmasıdır.\n Deneyden elde edilen bilgilere göre yaşantımızdaki ısı tasarrufunu artırmak, veriler ile günlük hayat şartlarının kolaylaştırmak amaçlanır.\n";
+        hipotez13 = "Mavi ve kırmızı çubukları alıp ısıtacağız.\n Hangisini daha iyi ısıyı ilettiğini gözlemleyip bunun üzerinden çeşitli çözümler üreteceğiz.\n";
+        arastirma13 = "Mavi ve kırmızı çubukların eşit koşullarda ısıyı aynı oranda iletip iletmediğini araştırdık.\n Isının renkler üzerinde etkisinin nasıl olduğuna dair çalışmalar yaptık.\n";
+        sonuc13 = "Başta 22° sıcaklığa sahip iki çubuk demir bulunmaktadır.\n Çubuk tutucularına yerleştirilen çubukları aynı koşullarda ısıtılır.\n Mavi boyalı demirin sıcaklığı 55° olarak ölçülmüştür.\n";
+        oneriler13 = "Mavi renge boyanan enerji nakil hatlarını ısı iletimleri tasarruf açısından daha etkilidir.\n Bu yüzden materyallerin mavi renge boyanması beklenir.\n ";
     }
 };
