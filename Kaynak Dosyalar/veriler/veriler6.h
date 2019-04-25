26898b4269f78ce4388126d390b908bad785b5a8
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-25 19:58:11 +0300

    Fatih \n

--- Kaynak Dosyalar/veriler/veriler6.h
+++ Kaynak Dosyalar/veriler/veriler6.h
@@ -10,9 +10,9 @@ string uygulanan_yontemler6;
 string ozet6;
 
 proje6(){
-string baslik6 ="İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ";
-string kullanılan_materyaller6= "Fotoğraf makinesi";
-string uygulanan_yontemler6 = "Gezi-Gözlem-Araştırma";
-string ozet6 = "İstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. Bu evlere gidilerek fotoğrafları çekildi. Evlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. ";	
+string baslik6 ="İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \n";
+string kullanılan_materyaller6= "Fotoğraf makinesi \n";
+string uygulanan_yontemler6 = "Gezi-Gözlem-Araştırma \n";
+string ozet6 = "İstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \n Bu evlere gidilerek fotoğrafları çekildi. \n Evlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n";	
 }
 };
