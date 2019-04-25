b6007f124eb180bbb43e9382f747aaac3dd08e9b
Gokhan1717 <49857128+Gokhan1717@users.noreply.github.com>
2019-04-25 21:10:50 +0300

    veriler12.h \n eklendi by GÖKHAN
    \n eklendi

--- Kaynak Dosyalar/veriler/veriler12.h
+++ Kaynak Dosyalar/veriler/veriler12.h
@@ -14,13 +14,13 @@ class proje12 {
     string oneri_ve_beklentiler12;
 
     proje12() {
-        baslik12 = "KASİSLERE SON";
-        materyaller12 = "1) Güneş enerjisi paneli 2) Lehim makinesi 3) Mıknatıs 4) Kaynak makinesi 5) Led ampül";
-        ozet12 = "Güçlü mıknatıslar sayesinde arabaları yavaşlatıp durduruyoruz. Yollara uyarı tabelası konulmalı çünkü yüksek hızla gelen bir aracın kaza yapma oranı çok yüksek. Projeden gözüken kazaların oranında azalma olacağına inanılıyor.Tüm ülkelerde yaşanan kırmızı ışık ihlalinden dolayı kazalar yaşamakta. Bizde bu kazaları önlemek için bu projeyi tasarladık.";
-        hipotez12 = "Günlük hayatta trafik esnasında trafik ışıkları çok büyük bir önleme sahiptir.Bu proje ile trafik ışıklarına dikkat etmeden sürücülerin nasıl durdurulacağına tespit etmek";
-        konu_arastirmasi12 = "Yayaların kırmızı ışık ihlali yapan sürücülerden şikayetçi olması gibi birçok sorun var. Projem sayesinde bu insanların sorunlarına açıklığı kavuşturabiliriz.Ayrıca araba süspansiyonlarının zarar görmesini de engelleyebiliriz.";
-        uygulanan_yontemler12 = "Hayatımız boyunca kurtulamadığımız trafik kazaları ailelerimizdeki kişileri aramızdan ayırıyor.İşte bu projenin asıl amacı da bunları önlemektir. İlk olarak yolun altındaki mıknatıs ile arabaları durduracağız ve güneş enerjisi sayesinde trafik ışıklarına elektrik sağlayacağız. Ayrıca düz yollarda kasislerin yerine de kullanılabiliir.";
-        sonuclar12 = "En başta elektromıknatıs ile denedik fakat teoride kolay ama pratikte yapılamayacağını anladık. Daha sonra elektromıknatıs yerine güçlü bir mıknatıs ile projeyi tamamladık. Projemizin başarılı olduğunu öğrendik.";
-        oneri_ve_beklentiler12 = "Bu projenin ileride başka bir ülkelerden talep edileceğine inanıyorum. Elektromıknatıs yerine normal mıknatıs kullanmanın daha etkili olduğunu gördüm.";
+        baslik12 = "KASİSLERE SON\n";
+        materyaller12 = "1) Güneş enerjisi paneli\n 2) Lehim makinesi\n 3) Mıknatıs\n 4) Kaynak makinesi\n 5) Led ampül\n";
+        ozet12 = "Güçlü mıknatıslar sayesinde arabaları yavaşlatıp durduruyoruz.\n Yollara uyarı tabelası konulmalı çünkü yüksek hızla gelen bir aracın kaza yapma oranı çok yüksek.\n Projeden gözüken kazaların oranında azalma olacağına inanılıyor.\nTüm ülkelerde yaşanan kırmızı ışık ihlalinden dolayı kazalar yaşamakta.\n Bizde bu kazaları önlemek için bu projeyi tasarladık.\n";
+        hipotez12 = "Günlük hayatta trafik esnasında trafik ışıkları çok büyük bir önleme sahiptir.\nBu proje ile trafik ışıklarına dikkat etmeden sürücülerin nasıl durdurulacağına tespit etmek.\n";
+        konu_arastirmasi12 = "Yayaların kırmızı ışık ihlali yapan sürücülerden şikayetçi olması gibi birçok sorun var.\n Projem sayesinde bu insanların sorunlarına açıklığı kavuşturabiliriz.\n Ayrıca araba süspansiyonlarının zarar görmesini de engelleyebiliriz.\n";
+        uygulanan_yontemler12 = "Hayatımız boyunca kurtulamadığımız trafik kazaları ailelerimizdeki kişileri aramızdan ayırıyor.\n İşte bu projenin asıl amacı da bunları önlemektir.\n İlk olarak yolun altındaki mıknatıs ile arabaları durduracağız ve güneş enerjisi sayesinde trafik ışıklarına elektrik sağlayacağız.\n Ayrıca düz yollarda kasislerin yerine de kullanılabiliir.\n";
+        sonuclar12 = "En başta elektromıknatıs ile denedik fakat teoride kolay ama pratikte yapılamayacağını anladık.\n Daha sonra elektromıknatıs yerine güçlü bir mıknatıs ile projeyi tamamladık.\n Projemizin başarılı olduğunu öğrendik.\n";
+        oneri_ve_beklentiler12 = "Bu projenin ileride başka bir ülkelerden talep edileceğine inanıyorum.\n Elektromıknatıs yerine normal mıknatıs kullanmanın daha etkili olduğunu gördüm.\n";
     }
 };
