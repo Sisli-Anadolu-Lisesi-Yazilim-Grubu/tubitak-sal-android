17db566cf937d11626979bc13afb75e0ae63a6f2
Gokhan1717 <49857128+Gokhan1717@users.noreply.github.com>
2019-04-25 21:56:20 +0300

    veriler12.h düzeltildi by  GÖKHAN
    Gereksiz boşluklar silindi

--- Kaynak Dosyalar/veriler/veriler12.h
+++ Kaynak Dosyalar/veriler/veriler12.h
@@ -15,12 +15,12 @@ class proje12 {
 
     proje12() {
         baslik12 = "KASİSLERE SON\n";
-        materyaller12 = "1) Güneş enerjisi paneli\n 2) Lehim makinesi\n 3) Mıknatıs\n 4) Kaynak makinesi\n 5) Led ampül\n";
-        ozet12 = "Güçlü mıknatıslar sayesinde arabaları yavaşlatıp durduruyoruz.\n Yollara uyarı tabelası konulmalı çünkü yüksek hızla gelen bir aracın kaza yapma oranı çok yüksek.\n Projeden gözüken kazaların oranında azalma olacağına inanılıyor.\nTüm ülkelerde yaşanan kırmızı ışık ihlalinden dolayı kazalar yaşamakta.\n Bizde bu kazaları önlemek için bu projeyi tasarladık.\n";
+        materyaller12 = "1) Güneş enerjisi paneli\n2) Lehim makinesi\n3) Mıknatıs\n4) Kaynak makinesi\n5) Led ampül\n";
+        ozet12 = "Güçlü mıknatıslar sayesinde arabaları yavaşlatıp durduruyoruz.\nYollara uyarı tabelası konulmalı çünkü yüksek hızla gelen bir aracın kaza yapma oranı çok yüksek.\nProjeden gözüken kazaların oranında azalma olacağına inanılıyor.\nTüm ülkelerde yaşanan kırmızı ışık ihlalinden dolayı kazalar yaşamakta.\nBizde bu kazaları önlemek için bu projeyi tasarladık.\n";
         hipotez12 = "Günlük hayatta trafik esnasında trafik ışıkları çok büyük bir önleme sahiptir.\nBu proje ile trafik ışıklarına dikkat etmeden sürücülerin nasıl durdurulacağına tespit etmek.\n";
-        konu_arastirmasi12 = "Yayaların kırmızı ışık ihlali yapan sürücülerden şikayetçi olması gibi birçok sorun var.\n Projem sayesinde bu insanların sorunlarına açıklığı kavuşturabiliriz.\n Ayrıca araba süspansiyonlarının zarar görmesini de engelleyebiliriz.\n";
-        uygulanan_yontemler12 = "Hayatımız boyunca kurtulamadığımız trafik kazaları ailelerimizdeki kişileri aramızdan ayırıyor.\n İşte bu projenin asıl amacı da bunları önlemektir.\n İlk olarak yolun altındaki mıknatıs ile arabaları durduracağız ve güneş enerjisi sayesinde trafik ışıklarına elektrik sağlayacağız.\n Ayrıca düz yollarda kasislerin yerine de kullanılabiliir.\n";
-        sonuclar12 = "En başta elektromıknatıs ile denedik fakat teoride kolay ama pratikte yapılamayacağını anladık.\n Daha sonra elektromıknatıs yerine güçlü bir mıknatıs ile projeyi tamamladık.\n Projemizin başarılı olduğunu öğrendik.\n";
-        oneri_ve_beklentiler12 = "Bu projenin ileride başka bir ülkelerden talep edileceğine inanıyorum.\n Elektromıknatıs yerine normal mıknatıs kullanmanın daha etkili olduğunu gördüm.\n";
+        konu_arastirmasi12 = "Yayaların kırmızı ışık ihlali yapan sürücülerden şikayetçi olması gibi birçok sorun var.\nProjem sayesinde bu insanların sorunlarına açıklığı kavuşturabiliriz.\nAyrıca araba süspansiyonlarının zarar görmesini de engelleyebiliriz.\n";
+        uygulanan_yontemler12 = "Hayatımız boyunca kurtulamadığımız trafik kazaları ailelerimizdeki kişileri aramızdan ayırıyor.\nİşte bu projenin asıl amacı da bunları önlemektir.\nİlk olarak yolun altındaki mıknatıs ile arabaları durduracağız ve güneş enerjisi sayesinde trafik ışıklarına elektrik sağlayacağız.\nAyrıca düz yollarda kasislerin yerine de kullanılabiliir.\n";
+        sonuclar12 = "En başta elektromıknatıs ile denedik fakat teoride kolay ama pratikte yapılamayacağını anladık.\nDaha sonra elektromıknatıs yerine güçlü bir mıknatıs ile projeyi tamamladık.\nProjemizin başarılı olduğunu öğrendik.\n";
+        oneri_ve_beklentiler12 = "Bu projenin ileride başka bir ülkelerden talep edileceğine inanıyorum.\nElektromıknatıs yerine normal mıknatıs kullanmanın daha etkili olduğunu gördüm.\n";
     }
 };
