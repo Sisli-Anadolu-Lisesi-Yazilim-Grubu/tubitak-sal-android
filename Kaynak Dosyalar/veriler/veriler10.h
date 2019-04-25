adcebde2c155671409338929a18170bb87dcd98a
Gokhan1717 <49857128+Gokhan1717@users.noreply.github.com>
2019-04-25 20:42:01 +0300

    veriler10.h \n eklendi by GÖKHAN
    \n eklendi

--- Kaynak Dosyalar/veriler/veriler10.h
+++ Kaynak Dosyalar/veriler/veriler10.h
@@ -16,14 +16,14 @@ class proje10 {
 		
 		proje10() {
 			
-		baslik10="YÜRÜYEN KRİSTALLER";
-		kullanilan_materyaller10 ="Birkaç adet piezo, Li-Po batarya, akım dönüştürücü için 4 adet diyot, kullanılmayan geniş tabanlı bir spor ayakkabı kullandık.";
-		uygulanan_yontem10 ="Devremizi  tamamladıktan sonra ayakkabının tabanına uygun bir şekilde yerleştirdik. Daha sonra yürüyüş yaparak test ettik. Bataryanın sarj olup olmadığından emin olmak için yürüyüşten sonra bir led ile kontrol ettik.";
-		analiz_sonuclari10 ="Test sonucuna göre piezolar ile elektrik üretmenin mümkün olduğunda kanıtlamış olduk. Daha iyi bir tasarım ve devre ile bu fikrin ileride, günlük hayatta önemli bir yeri olacağıma inanıyoruz.";
-		proje_ozeti10 ="Elektrik üretmenin alternarif yollarını ararken, piezo kristallerin üretebileceğini fark ettik. Bunun üzerine tasarladığımız ayakkabıda, bu yöntem ile günlük hareketlerimizden elektrik üreterek bunu kullanmayı amaçladık.";
-		hipotez10 ="Piezo kristalleri kullanarak, ekstra efor sarf etmeden enerji üretebilir miyiz ?";
-		konu_arastirmasi10 ="Genel olarak titreşim sensörü olarak kullanılan piezolar, üstüne basınç uygulandığına küçük bir miktarda elektrik üretiyor. Bu elektriği depolayıp, kullanma açısından pek çok çalışma olmadığından, kaynağımız sınırlı.";
-		sonuclar10 ="Piezo kristaller ilke elektrik üretip bu elektriği kullanabileceğimizi kanıtladık.";
-		oneri_ve_beklentiler10 ="Bu teknolojinin ileride kullanım alanının geniş olacağına inanıyoruz. Bu tasarım geliştirilirse kişinin günlük hayatında önemli yer kaplayacağını düşünüyoruz. Sadece ayakkabı ile kalmayıp, örneğin kalabalık yerledeki yollarda kullanılabilir.";
+		baslik10="YÜRÜYEN KRİSTALLER\n";
+		kullanilan_materyaller10 ="Birkaç adet piezo, Li-Po batarya, akım dönüştürücü için 4 adet diyot, kullanılmayan geniş tabanlı bir spor ayakkabı kullandık.\n";
+		uygulanan_yontem10 ="Devremizi  tamamladıktan sonra ayakkabının tabanına uygun bir şekilde yerleştirdik.\n Daha sonra yürüyüş yaparak test ettik.\n Bataryanın sarj olup olmadığından emin olmak için yürüyüşten sonra bir led ile kontrol ettik.\n";
+		analiz_sonuclari10 ="Test sonucuna göre piezolar ile elektrik üretmenin mümkün olduğunda kanıtlamış olduk.\n Daha iyi bir tasarım ve devre ile bu fikrin ileride, günlük hayatta önemli bir yeri olacağıma inanıyoruz.\n";
+		proje_ozeti10 ="Elektrik üretmenin alternarif yollarını ararken, piezo kristallerin üretebileceğini fark ettik.\n Bunun üzerine tasarladığımız ayakkabıda, bu yöntem ile günlük hareketlerimizden elektrik üreterek bunu kullanmayı amaçladık.\n";
+		hipotez10 ="Piezo kristalleri kullanarak, ekstra efor sarf etmeden enerji üretebilir miyiz ?\n";
+		konu_arastirmasi10 ="Genel olarak titreşim sensörü olarak kullanılan piezolar, üstüne basınç uygulandığına küçük bir miktarda elektrik üretiyor.\n Bu elektriği depolayıp, kullanma açısından pek çok çalışma olmadığından, kaynağımız sınırlı.\n";
+		sonuclar10 ="Piezo kristaller ilke elektrik üretip bu elektriği kullanabileceğimizi kanıtladık.\n";
+		oneri_ve_beklentiler10 ="Bu teknolojinin ileride kullanım alanının geniş olacağına inanıyoruz.\n Bu tasarım geliştirilirse kişinin günlük hayatında önemli yer kaplayacağını düşünüyoruz. Sadece ayakkabı ile kalmayıp, örneğin kalabalık yerledeki yollarda kullanılabilir.\n";
 		}
 };
