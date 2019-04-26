fbb43359dda1847693785debfb68d483e20ef981
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-26 12:09:50 +0300

    fatih metin ekledi

--- Kaynak Dosyaları/ Veriler/veriler4.h
+++ Kaynak Dosyaları/ Veriler/veriler4.h
@@ -8,6 +8,7 @@ string baslik4;
 string metin4;
 
 proje4(){
-metin4 = "İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \nKULLABILAN MATERYALLER\nFotoğraf makinesi \nUYGULANAN YÖNTEMLER\n Gezi-Gözlem-Araştırma \nÖZET\nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n ";	
+baslik4 = "İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \n";
+metin4 = "KULLANILAN MATERYALLER\nFotoğraf makinesi \nUYGULANAN YÖNTEMLER\n Gezi-Gözlem-Araştırma \nÖZET\nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n ";	
 }
 };
