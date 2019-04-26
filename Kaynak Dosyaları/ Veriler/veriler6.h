661ec371c56196e0d8f7470d202a0c4311ce27e1
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-26 13:51:11 +0300

    veriler6.h
    veriler 6 yeniden yazıldı

--- Kaynak Dosyaları/ Veriler/veriler6.h
+++ Kaynak Dosyaları/ Veriler/veriler6.h
@@ -9,8 +9,8 @@ public:
     
 
     proje6() {
-    baslik6 = "Almanca Sıfatların Öğretilmesinin Oyunlaştırılması";
-    metin6 = "•Kullanılan materyallerÖzgün oyunlar• Özet:İkinci yabancı dil olarak öğretilen Almanca derslerinde öğrenciler bazı zorluklar yaşamaktadır.Bu nedenle de Almancanın öğrenilmesi zor bir dil olduğu gibi bir ön yargı oluşmuştur. Öğrenciler özellikle Almanca sıfatlar ve sıfatların farklı sözcük türü olarak kullanılması ya da sıfattan türetilmiş fiilleri öğrenmekte zorluk yaşamaktadır.Bu zorlukları konu içeriğine uygun oyunların derste kullanılması ile en aza indirebiliriz.•Sonuçlar:Almancanın sıfatları ve sıfatların farklı sözcük türü olarak kullanılmasını ya da sıfattan türetilmiş fillerin derste içeriğine uygun konularla işlemenin öğrencilerin ikinci yabancı dil Almancaya ilgilerini arttırmakla birlikte bu konuların öğrenilmesini de kolaylaştırdığı ve öğrenme sürecini hızlandırdığı gözlemlenmiştir.";
+    baslik6 = "Almanca Sıfatların Öğretilmesinin Oyunlaştırılması\n";
+    metin6 = "•Kullanılan materyaller\nÖzgün oyunlar\n• Özet:İkinci yabancı dil olarak öğretilen Almanca derslerinde öğrenciler bazı zorluklar yaşamaktadır.\nBu nedenle de Almancanın öğrenilmesi zor bir dil olduğu gibi bir ön yargı oluşmuştur.\nÖğrenciler özellikle Almanca sıfatlar ve sıfatların farklı sözcük türü olarak kullanılması ya da sıfattan türetilmiş fiilleri öğrenmekte zorluk yaşamaktadır.\nBu zorlukları konu içeriğine uygun oyunların derste kullanılması ile en aza indirebiliriz.\n•Sonuçlar:Almancanın sıfatları ve sıfatların farklı sözcük türü olarak kullanılmasını ya da sıfattan türetilmiş fillerin derste içeriğine uygun konularla işlemenin öğrencilerin ikinci yabancı dil Almancaya ilgilerini arttırmakla birlikte bu konuların öğrenilmesini de kolaylaştırdığı ve öğrenme sürecini hızlandırdığı gözlemlenmiştir.\n";
     
     }
 };
