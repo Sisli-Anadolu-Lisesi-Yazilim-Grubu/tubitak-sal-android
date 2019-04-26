de502accfb832413d8253edee8f32da06abdf1a9
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-26 07:34:44 +0300

    Update and rename Kaynak Dosyalar/veriler/veriler9.h to  Kaynak Dosyaları/ Veriler/veriler9.h

---
+++ Kaynak Dosyaları/ Veriler/veriler9.h
@@ -0,0 +1,17 @@
+#include "namespace.h"
+
+using namespace TUBITAK;
+
+class proje9{
+	public:
+		string baslik9;
+		string apptionary9;
+		string konu_ozeti9;
+		string konu_arastirmasi9;
+		proje9(){
+			baslik9 = "APPTIONARY\n";
+			apptionary9 = "Bu projede, MEB`in 9.sınıf İngilizce öğretim programında yer alan 'TEENWISE' ders kitabı, uygulama oluşturulurken 'Adove Photoshop' ve 'App Inventor 2.0' programları kullanılmıştır.\nKitabın üniteleri incelenmiş ve telaffuz açısından zor olduğu görülen kelimeler belirlenmiştir.\nKelimelerin belirlenmesinde, derslerde sıkça yapılan telaffuz hataları göz önünde bulundurulmuştur ve yanlış telaffuz edildiği tespit edilen kelimeler bir havuza aktarılmıştır.\n";
+			konu_ozeti9 = "KONU ÖZETİ\nÜlkemizde yabancı dil eğitimi konusunda yenilikçi yöntemlerin kullanılması kaçınılmaz olmuştur.\nBu amaçla dil öğretimini daha eğlenceli ve kolay hale getirmek, öğrencilerin kelime haznesini geliştirilmesi için hem işitsel hem de görsel hafızaya hitap edecek şekilde tasarlanan teknolojik bir uygulama hazırladık.\n";
+			konu_arastirmasi9 = "KONU ARAŞTIRMASI\nDünya çapında ülkemizin de içinde bulunduğu 44 ülkede İngilizce yeterlilik testi uygulamış ve Türkiye;eğitim seviyesinin düşük olduğu Şili,Endonezya Suudi Arabistan gibi ülkelerin de gerisinde kalarak 43.Sıraya yerleşmiştir.\nFarklı Android sürümlerinde ve farklı çözünürlükteki cihazlarda denemeler yaparak uygulamanın sorunsuzluğunu ve işlerliği sınandı.\nAkabinde karşılaşılan problemler bertaraf edilmeye çalışıldı Kendi sınıfımızdan başlayarak, arkadaşlarımızın cihazlarına uygulamayı yükledik ve nasıl kullanacakları ile ilgili kısa bilgiler verdik ve sorunsuz bir şekilde uygulamayı kullanmalarını sağladık.\nŞu ana kadar uygulamayı kullananlar tarafından, kelimelerin alfabetik sıraya konulması, arayüzün göze hitap edici olması, kelimelerin yanındaki resimlerin sayısının artması gibi öneriler sunulmuştur ve uygulamanın içerdiği kelimelerin sayısının artması yönünde beklentiler dile getirilmiştir.\n";
+		}
+};
