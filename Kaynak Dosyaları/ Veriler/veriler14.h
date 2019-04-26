66de0d577944d7a1a5dc3caa1dbc71692f86676a
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-26 11:43:46 +0300

    metin14
    metin14

--- Kaynak Dosyaları/ Veriler/veriler14.h
+++ Kaynak Dosyaları/ Veriler/veriler14.h
@@ -5,24 +5,12 @@ using namespace TUBITAK;
 class proje14 {
     public:
     string baslik14;
-    string materyaller14;
-    string yontemler14;
-    string ozet14;
-    string soru14;
-    string hipotez14;
-    string sorular14;
-    string sonuclar14;
-    string oneriler14;
-
+    string metin14;
+    
     proje14() {
         baslik14 = "BİREYLERE DAYATILAN TOPLUMSAL CİNSİYET KALIPLARINA DAİR FARKINDALIK\n";
-        materyaller14 = "KULLANILAN MATERYALLER\nSoru kağıdı\n";
-        yontemler14 = "UYGULANAN YÖNTEMLER\nTümevarım Evren-Örneklem verilerin toplanması-Çözümü-Yorumlanması Yapılan analizlere göre belli meslekler belli cinsiyetlerle ilişkilendirmektedir.\n";
-        ozet14 = "ÖZET\nBu çalışmada, toplumsal yapıda kabul gören cinsiyet algısı araştırılmıştır.\nToplumsal cinsiyet biyolojik cinsiyetten farklı olarak, kadınların ve erkeklerin nasıl davranması gerektiği ile ilgili davranış kalıpları ve beklentiler ortaya sunar.\n";
-        soru14 = "SORU\nToplumsal cinsiyet belli davranış kalıpları yaratır mı ? Bu kalıplar, meslek seçimlerine yansır mı ?\n";
-        hipotez14 = "HİPOTEZ\nToplumsal cinsiyet kalıpları meslek seçimlerine yansımakta: bilim, felsefe, sanat, teknoloji gibi alanlar belli cinsiyetlerle ilişkilendirmektedir.\nToplumsal cinsiyet kalıplarını ve bu kalıpların kişilerde yarattığı algıyı ölçebilmek amacıyla özellikle kadına veya erkeğe atfedilen alanlarla ilgili sorular hazırlanmış; biri kadın biri erkek olmak üzere bu soruların her birine seçenekler oluşturulmuştur.\n";
-        sorular14 = "SORULAR\n1. Yuvarlak testereyi kim icat etmiştir?\n2.İlk zengin donanımlı bilgisayarı kim geliştirmiştir?\n3. Altın madalya kazanan dansçımız kimdir?\n4. Rio olimpiyatlarında atıcılık, binicilik dallarında birincilik kazanan sporcumuz kimdir?\n5.Dokuma tezgahını bulan kişi kimdir?\n6. Konuşan oyuncak bebeği icat eden kişi kimdir?\n7. 'Düşünme hakkınızı saklı tutun, yanlış düşünmek bile hiç düşünmemekten iyidir.'sözü hangi filozofa aittir?\n8. Apollo 11 uzay aracının sorunsuz bir şekilde iniş yapmasını sağlayan bilgisayar programcısı kimdir?\n ";
-        sonuclar14 = "SONUÇLAR\nSorularımızı toplamda 386 kişi yanıtladı: \n9.SINIF: 83 \n10.SINIF: 97 \n11.SINIF:121 \n12.SINIF: 85 KİŞİ SORULARI YANITLADI\nYukarıdaki grafikte görüldüğü üzere, 6.soruda büyük bir çoğunluk belirgin bir şekildi, konuşan oyuncak bebeğin mucidini kadın olarak düşünmüştür.\n";
-        oneriler14 = "ÖNERİLER VE BEKLENTİLER\n*TOPLUMSAL CİNSİYET KONUSUNDAKİ KALIP YARGILARIN KIRILMASI ADINA BİLİNÇLENDİRME ÇALIŞMALARI YAPILMALIDIR.\n*TOPLUMSAL CİNSİYET EŞİTSİZLİĞİNİN ÖNÜNE GEÇİLMELİDİR\n *OKULLARDA, HALK EĞİTİM MERKEZLERİNDE, BELEDİYELERDE, TOPLUMSAL CİNSİYET KONUSUNDA DERSLER KONMALI VE EĞİTİMLER VERİLMELİDİR.\";
+        metin14 = "KULLANILAN MATERYALLER\nSoru kağıdı\nUYGULANAN YÖNTEMLER\nTümevarım Evren-Örneklem verilerin toplanması-Çözümü-Yorumlanması Yapılan analizlere göre belli meslekler belli cinsiyetlerle ilişkilendirmektedir.\nÖZET\nBu çalışmada, toplumsal yapıda kabul gören cinsiyet algısı araştırılmıştır.\nToplumsal cinsiyet biyolojik cinsiyetten farklı olarak, kadınların ve erkeklerin nasıl davranması gerektiği ile ilgili davranış kalıpları ve beklentiler ortaya sunar.\nSORU\nToplumsal cinsiyet belli davranış kalıpları yaratır mı ? Bu kalıplar, meslek seçimlerine yansır mı ?\nHİPOTEZ\nToplumsal cinsiyet kalıpları meslek seçimlerine yansımakta: bilim, felsefe, sanat, teknoloji gibi alanlar belli cinsiyetlerle ilişkilendirmektedir.\nToplumsal cinsiyet kalıplarını ve bu kalıpların kişilerde yarattığı algıyı ölçebilmek amacıyla özellikle kadına veya erkeğe atfedilen alanlarla ilgili sorular hazırlanmış; biri kadın biri erkek olmak üzere bu soruların her birine seçenekler oluşturulmuştur.\nSORULAR\n1. Yuvarlak testereyi kim icat etmiştir?\n2.İlk zengin donanımlı bilgisayarı kim geliştirmiştir?\n3. Altın madalya kazanan dansçımız kimdir?\n4. Rio olimpiyatlarında atıcılık, binicilik dallarında birincilik kazanan sporcumuz kimdir?\n5.Dokuma tezgahını bulan kişi kimdir?\n6. Konuşan oyuncak bebeği icat eden kişi kimdir?\n7. 'Düşünme hakkınızı saklı tutun, yanlış düşünmek bile hiç düşünmemekten iyidir.'sözü hangi filozofa aittir?\n8. Apollo 11 uzay aracının sorunsuz bir şekilde iniş yapmasını sağlayan bilgisayar programcısı kimdir?\nSONUÇLAR\nSorularımızı toplamda 386 kişi yanıtladı: \n9.SINIF: 83 \n10.SINIF: 97 \n11.SINIF:121 \n12.SINIF: 85 KİŞİ SORULARI YANITLADI\nYukarıdaki grafikte görüldüğü üzere, 6.soruda büyük bir çoğunluk belirgin bir şekildi, konuşan oyuncak bebeğin mucidini kadın olarak düşünmüştür.\nÖNERİLER VE BEKLENTİLER\n*TOPLUMSAL CİNSİYET KONUSUNDAKİ KALIP YARGILARIN KIRILMASI ADINA BİLİNÇLENDİRME ÇALIŞMALARI YAPILMALIDIR.\n*TOPLUMSAL CİNSİYET EŞİTSİZLİĞİNİN ÖNÜNE GEÇİLMELİDİR\n *OKULLARDA, HALK EĞİTİM MERKEZLERİNDE, BELEDİYELERDE, TOPLUMSAL CİNSİYET KONUSUNDA DERSLER KONMALI VE EĞİTİMLER VERİLMELİDİR.\n ";
+       
+        
     }
 };
