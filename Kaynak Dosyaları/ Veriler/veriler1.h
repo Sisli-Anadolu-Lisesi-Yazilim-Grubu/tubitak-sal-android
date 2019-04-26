48efed44f823583340b8cca8a838130272ac7b4f
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-26 11:20:28 +0300

    metin değişkeni eklendi fatih
    fatih

--- Kaynak Dosyaları/ Veriler/veriler1.h
+++ Kaynak Dosyaları/ Veriler/veriler1.h
@@ -2,20 +2,11 @@
  using namespace TUBITAK;
 class proje1 {
 	public:
-		string baslik ; 
-		string kullanilan_materyaller ;
-		string uygulanan_yontemler ;
-		string analiz_sonuclari ;
-		string ozet; 
-		string oneriler_ve_beklentiler;
+		string baslik; 
+		string metin;
 		
 		proje1(){
 			baslik = "ŞİŞLİ'NİN DÜNÜ VE BUGÜNÜ \n";
-			kullanilan_materyaller = "KULLANILAN MATERYALLER\nFotoğraf Makinesi \n";
-			uygulanan_yontemler = "UYGULANAN YÖNTEMLER\nGezi - Gözlem - Araştırma \n";
-			analiz_sonuclari =  "ANALİZ SONUÇLARI\nŞişli de tarihi dokunun korunamadığı; AVM ve gökdelenlerin sayısının fazla olduğu, yeşil alanın az olduğu görülmüştür. \n";
-			ozet = "ÖZET\nİstanbul tarih boyunca önemli bir şehir \nolmuştur. \nŞişli de İstanbul'un önemli semtlerinden biridir. \nBu proje ile tarih boyunca Şişli'nin geçirdiği dönüşümü ortaya çıkarmayı amaçlıyoruz \n";
-			oneriler_ve_beklentiler = "ÖNERİLER VE BEKLENTİLER\nŞişli'de AVM ve gökdelenler çok olduğu için yüksek binaların yapılması önlenmelidir. \nYeşil alanların çoğaltılması gerekmektedir. \nOtopark sorununu çözmek için yer altına otopark yapılmalıdır. \nDikey mimari yerine yatay mimariye önem verilmelidir. \n";
-			
+		        metin = "KULLANILAN MATERYALLER\nFotoğraf Makinesi \nUYGULANAN YÖNTEMLER\nGezi - Gözlem - Araştırma \nANALİZ SONUÇLARI\nŞişli de tarihi dokunun korunamadığı; AVM ve gökdelenlerin sayısının fazla olduğu, yeşil alanın az olduğu görülmüştür. \nÖZET\nİstanbul tarih boyunca önemli bir şehir \nolmuştur. \nŞişli de İstanbul'un önemli semtlerinden biridir. \nBu proje ile tarih boyunca Şişli'nin geçirdiği dönüşümü ortaya çıkarmayı amaçlıyoruz \nÖNERİLER VE BEKLENTİLER\nŞişli'de AVM ve gökdelenler çok olduğu için yüksek binaların yapılması önlenmelidir. \nYeşil alanların çoğaltılması gerekmektedir. \nOtopark sorununu çözmek için yer altına otopark yapılmalıdır. \nDikey mimari yerine yatay mimariye önem verilmelidir. \n";
 		}
 };
