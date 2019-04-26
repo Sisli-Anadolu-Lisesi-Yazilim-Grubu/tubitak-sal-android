d04006c0ad83f2a1574049d1fb6c78b6e1315651
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-26 07:59:55 +0300

    Update and rename Kaynak Dosyalar/veriler/veriler18.h to  Kaynak Dosyaları/ Veriler/veriler18.h

---
+++ Kaynak Dosyaları/ Veriler/veriler18.h
@@ -0,0 +1,33 @@
+
+#include "namespace.h"
+using namespace TUBITAK;
+
+class proje18 {
+	public:
+		string baslik18;
+		string kullanilan_materyaller18;
+		string uygulanan_yontemler18;
+		string analiz_sonuclari18;
+		string ozet18;
+		string soru18;
+		string hipotez18;
+		string konu_arastirmasi18;
+		string sonuclar18;
+		string oneri_ve_beklentiler18;
+
+		
+		proje18() {
+		baslik18=" 'Annesiz Büyüyen Şairlerin Şiirlerindeki Hüzün'\n ";	
+		kullanilan_materyaller18="KULLANILAN MATERYALLER\nMukavva \nRenkli fon karton \nYapıştırıcı \nFotoğraf \nŞairlerin şiirlerindeki örnekler \nve \nalıntılar\n";
+		uygulanan_yontemler18="UYGULANAN YÖNTEMLER\nÖncelikle,annesiz büyüyen şairlerin kimler olduğunu araştırdık.\nDaha sonra bu şairlerin şiirlerinde en çok yoğunlaştırdıkları hüzün temasını inceledik.\nBu konuda yazılmış makalelere ulaştık.\nKaynak taramaları yaptık. Ardından ulaştığımız sonuçları,görsel malzemeler kullanarak bir sunum haline getirdik.\n";
+		analiz_sonuclari18="ANALİZ SONUÇLARI\nAnnenin küçük yaşta kaybedilmesi,bir çocuk için dünyada en yaralayıcı gerçekle yüz yüze kalmaktır.\nBunu yaşayan şairlerim 'hüzün' temasını şekillendiren en önemli olgulardan biri bu elim hadisedir.\nOldukça duyarlı geçen çocukluk günleri, annenin zamansız vedası, geçmişe duyulan derin özlem ve ömür boyu süren müzmin yalnızlık adeta onların ortak kaderidir.\nBu şairlerden bazıları, tam anlamıyla aile düzenine kavuşamaz; nihayetsiz gönül maceralarıyla dolu yalnız bir hayat sürerler.\nBazen 'sevgili' motifinin anneye alt vasıfları taşıdığı da görülmektedir.\n";
+		ozet18="ÖZET\nAnnesini küçük yaşta kaybeden ünlü şairlerimiz vardır.\nBu derin üzüntü, onların şiirlerin kimi zaman teması kimi zaman da arka planı olmuştur.\nÖyle ki; onların yalnızlığı seçmesinin,melal ile beslenmelerinin gizli sebebidir annesizlik.\nAnneye duyulan özlem, kimsenin yerini dolduramadığı büyük boşluğa ve daha derin ruh hallerine taşımıştır onları.\nFarklı cepheleriyle anne, bu şairlerin şahsiyetlerinde belirleyici rol oynadığı gibi kaleme aldıkları şiirlerde sıkça yer bulması bakımından da dikkat çekici olmuştur.\n";
+		soru18="SORU\nAnnesini küçük yaşta kaybeden şairlerin bu üzüntüsü, onların şiirlerine ne şekilde yansımıştır?\n";
+		hipotez18="HİPOTEZ\nAnnesiz büyüyen şairlerin şiirlerinde derin bir hüzün var vardır.\n";
+		konu_arastirmasi18="KONU ARAŞTIRMASI\nTürk edebiyatında iz bırakmış önemli şairlerin hayatlarını derinden etkileyen olayları araştırmak ve bu olayların onların sanat anlayışlarına özellikle de şiirlerine ne gibi etkileri olduğunu tespit etmek.\n";
+                sonuclar18="SONUÇLAR\nTürk edebiyatında iz bırakmış önemli şairlerin erken yaşta annelerini yitirmeleri ve bu ayrılıktan sonra yaşadıkları boşluk hissi, onları büyük ölçüde etkilemiştir.\nBu sanatçıların şiirlerinde 'anne'nin çoğu kez yad edildiğini yahut ölüm ve hüzün temalarıyla kendini gösterdiğini tespit etmekteyiz.\nBu kutsal varlık onlar için;bir zamanlar merhamet dolu kalbiyle huzur ve güven bahşeden fakat erken yitirilmiş bir sığınaktır.\nEksikliğini hiçbir zaman kapatılmamış, bu özlem; şiirlerde imgesel bir boyutta ortaya çıkmıştır.\nBu değerli varlığı toprağa emanet eden yahut onun şefkatinden yoksun büyüyen şair için, artık toprak da kutsaldır.\nDolayısıyla ölüm, anlamda anneye dönüştür.\n";
+                oneri_ve_beklentiler18="ÖNERİLER VE BEKLENTİLER\nBu araştırma - inceleme çalışması, Türk edebiyatının önde gelen şairlerinin şiirlerine farklı bir bakış açısı kazandırabilir.\nAyrıca anne sevgisine veya anneye dair hatırlara edebi eserde yer verilmesi hususunda şiir türünün bir adım önde olduğu bilinmelidir.\n";
+		}
+};
+
+
