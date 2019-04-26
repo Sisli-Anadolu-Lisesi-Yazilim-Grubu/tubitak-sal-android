1a9588cc7152ec74a6680755e67d6a845cfb522e
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-26 11:29:17 +0300

    metin eklendi fatih

--- Kaynak Dosyaları/ Veriler/veriler4.h
+++ Kaynak Dosyaları/ Veriler/veriler4.h
@@ -5,14 +5,9 @@ using namespace TUBITAK;
 class proje4{	
 public:
 string baslik4;
-string kullanılan_materyaller4;
-string uygulanan_yontemler4;
-string ozet4;
+string metin4;
 
 proje4(){
-string baslik4 ="İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \n";
-string kullanılan_materyaller4= "KULLABILAN MATERYALLER\nFotoğraf makinesi \n";
-string uygulanan_yontemler4 = "UYGULANAN YÖNTEMLER\n Gezi-Gözlem-Araştırma \n";
-string ozet4 = "ÖZET\nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n ";	
+metin4 = "İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \nKULLABILAN MATERYALLER\nFotoğraf makinesi \nUYGULANAN YÖNTEMLER\n Gezi-Gözlem-Araştırma \nÖZET\nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n ";	
 }
 };
