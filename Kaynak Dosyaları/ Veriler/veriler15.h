e31f2c891b156a480802df1946f0ee1af43fe0c9
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-26 07:51:38 +0300

    Update and rename Kaynak Dosyalar/veriler/veriler15.h to  Kaynak Dosyaları/ Veriler/veriler15.h

---
+++ Kaynak Dosyaları/ Veriler/veriler15.h
@@ -0,0 +1,24 @@
+#include "namespace.h"
+
+using namespace TUBITAK;
+
+class proje15 {
+    public:
+    string baslik15;
+    string materyaller15;
+    string metot15;
+    string ozet15;
+    string soru15;
+    string hipotez15;
+    string sonuc_ve_tartisma15;
+
+    proje15() {
+        baslik15 = "HEM BAKIMLI HEM SAĞLIKLI OL\n";
+        materyaller15 = "KULLANILAN MATERYALLER\nAgar Besiyer(Katı-Standart) \nKarbonat \nLimon Kabuğu \nAvokado \nLavanta Yağı \nBeher(100 ml) \nKaşık \nAlkol \nSpatül\n";
+        metot15 = "METOT\nGüzel kokmak ve bakımlı olmak amacıyla yaptığımız deneyin ilk adımı bunun için en mantıklı maddeyi araştırmaktı.\nAraştırma sonucunda karbonatın bu proje için en uygun madde olduğunu kararlaştırdık.\nBirinci kaba karbonat koyduk.\nİkinci kaba limon ve karbonat, üçüncü kaba avokado yağı ve karbonat, dördüncü kaba ise lavanta yağı ve karbonatı karıştırarak koyduk.\nSteril ortamda bir arkadaşımızdan spatül  ile sıyırarak aldığımız teri, agarlı katı besiyerine yayma yöntemi ile ektik.\nElde ettiğimiz bu karışımları yine spatül ile arkadaşımızdan aldığımız ter örneğiyle ayrı ayrı karıştırarak, ayrı petrilerdeki agar besiyerine aynı yöntemle ektik.\n";
+        ozet15 = "ÖZET\nBiz insanlar her zaman güzel kokmak ve bakımlı olmak isteriz.\nKişisel bakım sadece estetik açıdan değil sağlık açısından da önemlidir.\nBunun için çeşitli kişisel bakım ürünleri kullanırız.\nBu ürünlerden en sık kullandıklarımzdan biri de koltuk altı ROLL ON'lardır.\nBu ürünlerin bir çoğu insanlara zarar verir.\nBiz de bu konuya bir çözüm aradık.\nAraştırmalarımız sonunda karbonatın antibakteriyel ve koku giderici bir madde olduğunu öğrendik ve bu konuda en etkili çözümün karbonat olabileceğini düşündük.\nBu projedeki amacımız insan vücuduna, ozon tabakasına ve doğaya zarar vermeden; hem ter kokusunu önlemek hem de güzel kokmaktır.\n";
+        soru15 = "SORU\nDeodorant ve Roll On yerine ne kullanabilrim?\n";
+        hipotez15 = "HİPOTEZ\nKarbonat koku giderici olarak kullanılabilir.\n";
+        sonuc_ve_tartisma15 = "SONUÇ VE TARTIŞMA\nDeney sonucunda, karbonatsız petride üreme yoğun bir şekilde görülürken, karbonat karışımlarını koyduğumuz petrilerde üremenin nispeten az olduğu görüldü.\nBu sonuçlar mevcut laboratuvar koşullarımız daha steril olabilseydi çok daha az üreme olacağını düşündürmektedir.\nBu deneyden yola çıkarak ter kokusunu önlemede standart Roll-On'lar yerine karbonat vb. antimikrobiyal maddelerin iyi birer alternatif olabileceği söylenebilir.\nGüzel kokmak içinse limon kabuğu, avokado yağı, lavanta esansı gibi birçok koku verici madde kullanılabilir.\n";
+    }
+};
