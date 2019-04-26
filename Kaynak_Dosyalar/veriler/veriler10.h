52d50ce4183b3a2ae38c65f2ba998e4d90998d54
Samet732 <SametHUB@732.com>
2019-04-26 19:18:11 +0300

    CREATED FİLE

---
+++ Kaynak_Dosyalar/veriler/veriler10.h
@@ -0,0 +1,16 @@
+#include "Kaynak_Dosyalar/namespace.h"
+
+using namespace TUBITAK;
+
+class proje10 {
+	public:
+		QString baslik10;
+		QString metin10;
+	
+		proje10() {
+			
+		baslik10="YÜRÜYEN KRİSTALLER\n";
+		metin10 = "KULLANILAN MATERYALLER\nBirkaç adet piezo \nLi-Po batarya \nakım dönüştürücü için 4 adet diyot \nkullanılmayan geniş tabanlı bir spor ayakkabı kullandık.\n KULLANILAN YÖNTEMLER \nDevremizi  tamamladıktan sonra ayakkabının tabanına uygun bir şekilde yerleştirdik.\nDaha sonra yürüyüş yaparak test ettik.\nBataryanın sarj olup olmadığından emin olmak için yürüyüşten sonra bir led ile kontrol ettik.\n ANALİZ SONUÇLARI\nTest sonucuna göre piezolar ile elektrik üretmenin mümkün olduğunda kanıtlamış olduk.\nDaha iyi bir tasarım ve devre ile bu fikrin ileride, günlük hayatta önemli bir yeri olacağıma inanıyoruz.\n PROJE ÖZETİ\nElektrik üretmenin alternarif yollarını ararken, piezo kristallerin üretebileceğini fark ettik.\nBunun üzerine tasarladığımız ayakkabıda, bu yöntem ile günlük hareketlerimizden elektrik üreterek bunu kullanmayı amaçladık.\n HİPOTEZ\nPiezo kristalleri kullanarak, ekstra efor sarf etmeden enerji üretebilir miyiz ?\n KONU ARAŞTIRMASI\nGenel olarak titreşim sensörü olarak kullanılan piezolar, üstüne basınç uygulandığına küçük bir miktarda elektrik üretiyor.\nBu elektriği depolayıp, kullanma açısından pek çok çalışma olmadığından, kaynağımız sınırlı.\n SONUÇLAR\nPiezo kristaller ilke elektrik üretip bu elektriği kullanabileceğimizi kanıtladık.\n ÖNERİLER VE BEKLENTİLER \nBu teknolojinin ileride kullanım alanının geniş olacağına inanıyoruz.\nBu tasarım geliştirilirse kişinin günlük hayatında önemli yer kaplayacağını düşünüyoruz.\nSadece ayakkabı ile kalmayıp, örneğin kalabalık yerledeki yollarda kullanılabilir.\n ";
+		
+		}
+};
