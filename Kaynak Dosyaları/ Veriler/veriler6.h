73e7ef65302f6097895306a00c9eb1495271e1b0
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-26 07:28:40 +0300

    Update and rename Kaynak Dosyalar/veriler/veriler6.h to  Kaynak Dosyaları/ Veriler/veriler6.h

---
+++ Kaynak Dosyaları/ Veriler/veriler6.h
@@ -0,0 +1,18 @@
+#include "namespace.h"
+
+using namespace TUBITAK;
+
+class proje6{	
+public:
+string baslik6;
+string kullanılan_materyaller6;
+string uygulanan_yontemler6;
+string ozet6;
+
+proje6(){
+string baslik6 ="İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \n";
+string kullanılan_materyaller6= "KULLANILAN MATERYALLER\nFotoğraf makinesi \n";
+string uygulanan_yontemler6 = "UYGULANAN YÖNTEMLER\nGezi-Gözlem-Araştırma \n";
+string ozet6 = "ÖZET \nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n";	
+}
+};
