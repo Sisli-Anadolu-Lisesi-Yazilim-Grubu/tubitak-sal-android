3004063a3d88adcb1ad262d6bc009cb451306043
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-26 12:11:43 +0300

    fatih met ekledi

--- Kaynak Dosyaları/ Veriler/veriler6.h
+++ Kaynak Dosyaları/ Veriler/veriler6.h
@@ -8,6 +8,7 @@ string baslik6;
 string metin6;
 
 proje6(){
-metin6 = "İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \nKULLANILAN MATERYALLER\nFotoğraf makinesi \nUYGULANAN YÖNTEMLER\nGezi-Gözlem-Araştırma \nÖZET \nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n";	
+baslik6 = "İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \n";
+metin6 = "KULLANILAN MATERYALLER\nFotoğraf makinesi \nUYGULANAN YÖNTEMLER\nGezi-Gözlem-Araştırma \nÖZET \nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n";	
 }
 };
