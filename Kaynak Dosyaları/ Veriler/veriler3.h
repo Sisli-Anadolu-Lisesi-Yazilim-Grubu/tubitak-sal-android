c22e32ecbdce54c4cb6f5e115849432751f06be1
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-26 11:26:27 +0300

    metin d. eklendi fatih

--- Kaynak Dosyaları/ Veriler/veriler3.h
+++ Kaynak Dosyaları/ Veriler/veriler3.h
@@ -4,25 +4,12 @@ class proje3 {
 	
 	public:
 		string baslik3;
-		string kullanilan_malzemeler3;
-		string uygulana_yontemler3;
-		string analiz_sonuclari3;
-		string proje_ozeti3;
-		string hipotez3;
-		string konu_arastirmasi3;
-		string sonuclar3;
-		string beklenti_ve_onerileri3;
+		string metin3;
 		
 		proje3(){
 			
 			baslik3 = "İDEAL SU";
 			kullanilan_malzemeler3 = "KULLANILAN MATERYALLER\n3 adet begonya \npH ölçer \nSaksı \nSu \nSirke \n";
-			uygulama_yontemler3 = "UYGULAMA YÖNTEMLER\nSuyun pH aralığının bitki üzerindeki etkilerini gözlemlemek için ikişer öğrenci seçildi. \nBu projede 5.5 ve 7 pH aralığındaki sulardan hangisinin bitki üzerinde en verimli etki bıraktığı araştırıldı. \n3 ayrı bitki alınarak her birine farklı pH aralığında sular verildi. \nBitklerin zarar görmemesi için doğal bir asit olan sirke kullanıldı. \npH aralıkları 5.5, 6.5 ve 7 olarak belirlendi. \nHer 3 çiçeğin de aynı sıcaklık, nem, ışık koşullarında olması sağlandı. \nÇiçekler belirli aralıklarla ve düzenli olarak sulandı. \nÇiçeklerdeki değişimler not edildi. \n";
-			analiz_sonuclari3 = "ANALİZ SONUÇLARI\nGözlem sonunda notlar karşılaştırıldı. \nBir buçuk ayın sonunda asidik suyun bitkinin yapraklarında, gövdesinde ve taç yapraklarında solmalar olduğu gözlemlendi. \n";
-			proje_ozeti3 = "ÖZET\nKendimize Dünya'daki her çeşit görünüşe, kokuya ve duyuya sahip bitkiye en uygun pH dereceli sular sulama yapılarak hem onların hayatta kalmalarını sağlayıp hem de bitkilerin yetiştiği toprağın verimliliğin arttığını ispat etme amacını hedefleyerek projeye başladık. \npH değerlerini ayarladığımız sularla deney gerçekleştirdik. \npH değişimlerinin bitkiler üzerindeki etkisini gözlemlemiş olduk. \n";
-			hipotez3 = "HİPOTEZ\nSulama suyunun pH derecesi bitki gelişiminde etkilidir. \n";
-			konu_arastirmasi3 = "KONU ARAŞTIRMASI\nDünyanın farklı coğrafyalarındaki iklim koşulları suyu da etkiliyor. \nSu hayatımızın her alanında mutlak bir önem taşıyor. \nFarklı pH aralığındaki suların bitkilerin gelişimi üzerindeki etkisini belirleyebilirsek çok daha verimli bitkiler elde edebiliriz. \n";
-			sonuclar3 = "SONUÇLAR\n20.01.2019 tarihinde üç adet begonya bitkisi alındı. \nŞubat ayının sonunda 5.5 ve 6.5 pH değerlerindeki sularla sulanan çiçeklerde; solmalar, kırılmalar gözlemlendi. \nDeney sonucunda bitkilerden asidik sularla sulananların öldüğü, nötr su ile sulananların yaşadığı gözlemlendi. \n";
-			beklenti_ve_oneriler3 = "BEKLENTİ VE ÖNERİLER\nProjeden beklentimiz asidik suyun da bitkinin gelişiminde olumlu rol oynamasıydı. \nBitkilerin gelişimi için nötr su daha faydalıdır. \n";
+			metin3 = "UYGULANAN YÖNTEMLER\nSuyun pH aralığının bitki üzerindeki etkilerini gözlemlemek için ikişer öğrenci seçildi. \nBu projede 5.5 ve 7 pH aralığındaki sulardan hangisinin bitki üzerinde en verimli etki bıraktığı araştırıldı. \n3 ayrı bitki alınarak her birine farklı pH aralığında sular verildi. \nBitklerin zarar görmemesi için doğal bir asit olan sirke kullanıldı. \npH aralıkları 5.5, 6.5 ve 7 olarak belirlendi. \nHer 3 çiçeğin de aynı sıcaklık, nem, ışık koşullarında olması sağlandı. \nÇiçekler belirli aralıklarla ve düzenli olarak sulandı. \nÇiçeklerdeki değişimler not edildi. \nANALİZ SONUÇLARI\nGözlem sonunda notlar karşılaştırıldı. \nBir buçuk ayın sonunda asidik suyun bitkinin yapraklarında, gövdesinde ve taç yapraklarında solmalar olduğu gözlemlendi. \nÖZET\nKendimize Dünya'daki her çeşit görünüşe, kokuya ve duyuya sahip bitkiye en uygun pH dereceli sular sulama yapılarak hem onların hayatta kalmalarını sağlayıp hem de bitkilerin yetiştiği toprağın verimliliğin arttığını ispat etme amacını hedefleyerek projeye başladık. \npH değerlerini ayarladığımız sularla deney gerçekleştirdik. \npH değişimlerinin bitkiler üzerindeki etkisini gözlemlemiş olduk. \nHİPOTEZ\nSulama suyunun pH derecesi bitki gelişiminde etkilidir. \nKONU ARAŞTIRMASI\nDünyanın farklı coğrafyalarındaki iklim koşulları suyu da etkiliyor. \nSu hayatımızın her alanında mutlak bir önem taşıyor. \nFarklı pH aralığındaki suların bitkilerin gelişimi üzerindeki etkisini belirleyebilirsek çok daha verimli bitkiler elde edebiliriz. \nSONUÇLAR\n20.01.2019 tarihinde üç adet begonya bitkisi alındı. \nŞubat ayının sonunda 5.5 ve 6.5 pH değerlerindeki sularla sulanan çiçeklerde; solmalar, kırılmalar gözlemlendi. \nDeney sonucunda bitkilerden asidik sularla sulananların öldüğü, nötr su ile sulananların yaşadığı gözlemlendi. \nBEKLENTİ VE ÖNERİLER\nProjeden beklentimiz asidik suyun da bitkinin gelişiminde olumlu rol oynamasıydı. \nBitkilerin gelişimi için nötr su daha faydalıdır. \n";
 		}
 		};
