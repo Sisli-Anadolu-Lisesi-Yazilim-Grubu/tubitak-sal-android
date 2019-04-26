1e44d25ef57b4778cffbbebf998971811927ac79
FurkanB190803 <49917698+FurkanB190803@users.noreply.github.com>
2019-04-26 07:38:32 +0300

    Update and rename Kaynak Dosyalar/veriler/veriler10.h to  Kaynak Dosyaları/ Veriler/veriler10.h

---
+++ Kaynak Dosyaları/ Veriler/veriler10.h
@@ -0,0 +1,29 @@
+#include "namespace.h"
+
+using namespace TUBITAK;
+
+class proje10 {
+	public:
+		string baslik10;
+		string kullanilan_materyaller10;
+		string uygulanan_yontem10;
+		string analiz_sonuclari10;
+		string proje_ozeti10;
+		string hipotez10;
+		string konu_arastirmasi10;
+		string sonuclar10;
+		string oneri_ve_beklentiler10;
+		
+		proje10() {
+			
+		baslik10="YÜRÜYEN KRİSTALLER\n";
+		kullanilan_materyaller10 ="KULLANILAN MATERYALLER\nBirkaç adet piezo \nLi-Po batarya \nakım dönüştürücü için 4 adet diyot \nkullanılmayan geniş tabanlı bir spor ayakkabı kullandık.\n";
+		uygulanan_yontem10 ="KULLANILAN YÖNTEMLER \nDevremizi  tamamladıktan sonra ayakkabının tabanına uygun bir şekilde yerleştirdik.\nDaha sonra yürüyüş yaparak test ettik.\nBataryanın sarj olup olmadığından emin olmak için yürüyüşten sonra bir led ile kontrol ettik.\n";
+		analiz_sonuclari10 ="ANALİZ SONUÇLARI\nTest sonucuna göre piezolar ile elektrik üretmenin mümkün olduğunda kanıtlamış olduk.\nDaha iyi bir tasarım ve devre ile bu fikrin ileride, günlük hayatta önemli bir yeri olacağıma inanıyoruz.\n";
+		proje_ozeti10 ="PROJE ÖZETİ\nElektrik üretmenin alternarif yollarını ararken, piezo kristallerin üretebileceğini fark ettik.\nBunun üzerine tasarladığımız ayakkabıda, bu yöntem ile günlük hareketlerimizden elektrik üreterek bunu kullanmayı amaçladık.\n";
+		hipotez10 ="HİPOTEZ\nPiezo kristalleri kullanarak, ekstra efor sarf etmeden enerji üretebilir miyiz ?\n";
+		konu_arastirmasi10 ="KONU ARAŞTIRMASI\nGenel olarak titreşim sensörü olarak kullanılan piezolar, üstüne basınç uygulandığına küçük bir miktarda elektrik üretiyor.\nBu elektriği depolayıp, kullanma açısından pek çok çalışma olmadığından, kaynağımız sınırlı.\n";
+		sonuclar10 ="SONUÇLAR\nPiezo kristaller ilke elektrik üretip bu elektriği kullanabileceğimizi kanıtladık.\n";
+		oneri_ve_beklentiler10 ="ÖNERİLER VE BEKLENTİLER \nBu teknolojinin ileride kullanım alanının geniş olacağına inanıyoruz.\nBu tasarım geliştirilirse kişinin günlük hayatında önemli yer kaplayacağını düşünüyoruz.\nSadece ayakkabı ile kalmayıp, örneğin kalabalık yerledeki yollarda kullanılabilir.\n";
+		}
+};
