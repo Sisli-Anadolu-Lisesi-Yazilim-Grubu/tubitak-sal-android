d6a6d080d318f611dfef517af31b8bec1b259280
afsarkapagan <49944086+afsarkapagan@users.noreply.github.com>
2019-04-26 11:32:43 +0300

    metin d eklendi fatih
    maltunç

--- Kaynak Dosyaları/ Veriler/veriler6.h
+++ Kaynak Dosyaları/ Veriler/veriler6.h
@@ -5,14 +5,9 @@ using namespace TUBITAK;
 class proje6{	
 public:
 string baslik6;
-string kullanılan_materyaller6;
-string uygulanan_yontemler6;
-string ozet6;
+string metin6;
 
 proje6(){
-string baslik6 ="İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \n";
-string kullanılan_materyaller6= "KULLANILAN MATERYALLER\nFotoğraf makinesi \n";
-string uygulanan_yontemler6 = "UYGULANAN YÖNTEMLER\nGezi-Gözlem-Araştırma \n";
-string ozet6 = "ÖZET \nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n";	
+metin6 = "İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ \nKULLANILAN MATERYALLER\nFotoğraf makinesi \nUYGULANAN YÖNTEMLER\nGezi-Gözlem-Araştırma \nÖZET \nİstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi. \nBu evlere gidilerek fotoğrafları çekildi. \nEvlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi. \n";	
 }
 };
