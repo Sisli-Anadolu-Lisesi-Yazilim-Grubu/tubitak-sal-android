7042ee8daf4ac5bbcbb950acddbccefadcd4d800
tatarmehmed <45494918+saldogaproje@users.noreply.github.com>
2019-05-02 09:07:46 +0300

    Update veriler12.h

--- Kaynak_Dosyalar/veriler/veriler12.h
+++ Kaynak_Dosyalar/veriler/veriler12.h
@@ -10,6 +10,6 @@ QString yapan12;
 proje12(){
 baslik12 = "İSTANBUL`DA YAŞAMIŞ EDEBİYATÇILAR VE EVLERİ";
 metin12 = "KULLANILAN MATERYALLER<br><br>Fotoğraf makinesi<br><br>UYGULANAN YÖNTEMLER<br><br>Gezi-Gözlem-Araştırma<br><br>ÖZET<br><br>İstanbul`da yaşamış edebiyatçıların evlerinin yerleri araştırılarak tespit edildi.<br>Bu evlere gidilerek fotoğrafları çekildi.<br>Evlerin ve edebiyatçıların fotoğrafları bir araya getirilerek bir şiiriyle birleştirildi.<br>";
-yapan12="<br><br>YAPANLAR<br><br>Nur AKYÜZ<br>Efsun Gönül DEMİRCAN<br>Buse AYDEMİR<br>İrem YORULMAZ<br>Mustafa Sezer YERLİ<br> Elif TAN<br> Ela ALİŞİR<br> Miray Hazal CANIBEK<br> Hasret ORĞAN<br> Elif Zehra KAM<br> Beyza ÇELEBİ<br> Dila ŞAHİN<br>"
+yapan12="<br><br>YAPANLAR(11/C)<br><br>Nur AKYÜZ<br>Efsun Gönül DEMİRCAN<br>Buse AYDEMİR<br>İrem YORULMAZ<br>Mustafa Sezer YERLİ<br> Elif TAN<br> Ela ALİŞİR<br> Miray Hazal CANIBEK<br> Hasret ORĞAN<br> Elif Zehra KAM<br> Beyza ÇELEBİ<br> Dila ŞAHİN(11/D)<br>"
 }
 };
