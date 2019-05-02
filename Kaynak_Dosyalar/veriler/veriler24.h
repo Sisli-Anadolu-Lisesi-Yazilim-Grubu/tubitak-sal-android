806ad2e395236cd42cccf22b8c2c58a8218f02c5
tatarmehmed <45494918+saldogaproje@users.noreply.github.com>
2019-05-02 20:35:07 +0300

    Update veriler24.h

--- Kaynak_Dosyalar/veriler/veriler24.h
+++ Kaynak_Dosyalar/veriler/veriler24.h
@@ -11,6 +11,6 @@ public:
     proje24() {
     baslik24 = "Almanca Sıfatların Öğretilmesinin Oyunlaştırılması";
     metin24 = "Kullanılan materyaller<br><br>Özgün oyunlar<br><br>ÖZET:<br><br>İkinci yabancı dil olarak öğretilen Almanca derslerinde öğrenciler bazı zorluklar yaşamaktadır.<br>Bu nedenle de Almancanın öğrenilmesi zor bir dil olduğu gibi bir ön yargı oluşmuştur.<br>Öğrenciler özellikle Almanca sıfatlar ve sıfatların farklı sözcük türü olarak kullanılması ya da sıfattan türetilmiş fiilleri öğrenmekte zorluk yaşamaktadır.<br>Bu zorlukları konu içeriğine uygun oyunların derste kullanılması ile en aza indirebiliriz.<br><br>SONUÇLAR:<br><br>Almancanın sıfatları ve sıfatların farklı sözcük türü olarak kullanılmasını ya da sıfattan türetilmiş fillerin derste içeriğine uygun konularla işlemenin öğrencilerin ikinci yabancı dil Almancaya ilgilerini arttırmakla birlikte bu konuların öğrenilmesini de kolaylaştırdığı ve öğrenme sürecini hızlandırdığı gözlemlenmiştir.<br>";
-    yapan24="<br><br>YAPANLAR<br><br>";
+    yapan24="<br><br>YAPANLAR<br><br> Ozan KARATEPE<br> Yaren DEMİREL<br>";
     }
 };
