4f8e7e6f5b112cd7e311804da8d8ed5bc97871c1
Samet732 <SametHUB@732.com>
2019-07-06 14:53:22 +0300

    update

--- Kaynak_Dosyalar/veriler/veriler1.h
+++ Kaynak_Dosyalar/veriler/veriler1.h
@@ -11,6 +11,6 @@ class TUBITAK::proje1 {
 		proje1(){
             baslik1 = "ŞİŞLİ'NİN DÜNÜ VE BUGÜNÜ";
             metin1 = "KULLANILAN MATERYALLER\n\nFotoğraf Makinesi \n\nUYGULANAN YÖNTEMLER\n\nGezi - Gözlem - Araştırma \n\nANALİZ SONUÇLARI\n\nŞişli de tarihi dokunun korunamadığı; AVM ve gökdelenlerin sayısının fazla olduğu, yeşil alanın az olduğu görülmüştür.\n \nÖZET\n\nİstanbul tarih boyunca önemli bir şehir olmuştur. \nŞişli de İstanbul'un önemli semtlerinden biridir. \nBu proje ile tarih boyunca Şişli'nin geçirdiği dönüşümü ortaya çıkarmayı amaçlıyoruz \n\nÖNERİLER VE BEKLENTİLER\n\nŞişli'de AVM ve gökdelenler çok olduğu için yüksek binaların yapılması önlenmelidir. \nYeşil alanların çoğaltılması gerekmektedir. \nOtopark sorununu çözmek için yer altına otopark yapılmalıdır. \nDikey mimari yerine yatay mimariye önem verilmelidir. \n";
-            yapan1 = "\n\nYAPANLAR\n\n-- Bilgi Mevcut Değil --<br>";
+            yapan1 = "\n\nYAPANLAR\n\n-- Bilgi Mevcut Değil --\n";
 		}
 };
